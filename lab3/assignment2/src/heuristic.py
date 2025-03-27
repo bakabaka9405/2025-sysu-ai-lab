920585e1aeae7370ea5f79da03368f7900eb6d0f
bakabaka9405 <2677423642@qq.com>
2025-03-27 22:54:47 +0800

    feat: lab 3.2 增加 h 值的乘方增强

--- lab3/assignment2/src/heuristic.py
+++ lab3/assignment2/src/heuristic.py
@@ -1,39 +1,61 @@
-def manhattan(ratio: float):
+from collections.abc import Callable
+from typing import Union
+
+
+def multiple_aug(ratio: float):
+	def aug(x: int) -> float:
+		return ratio * x
+
+	return aug
+
+
+def power_aug(ratio: float):
+	def aug(x: int) -> float:
+		return ratio**x
+
+	return aug
+
+
+def manhattan(aug: Callable[[int], Union[int, float]]):
 	"""
 	曼哈顿距离
 	"""
+
 	def h(s: list[int]) -> int:
-		return int(sum([abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3)) for i in range(16) if s[i] != 15]) * ratio)
+		return int(aug(sum([abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3)) for i in range(16) if s[i] != 15])))
 
 	return h
 
 
-def weighted_manhattan(ratio: float):
+def weighted_manhattan(aug: Callable[[int], Union[int, float]]):
 	"""
 	带权曼哈顿距离
 	理论上能刺激 A* 算法按 1,2,3... 的顺序还原 puzzle，从而提高在宽松步数限制下的计算效率
 	"""
+
 	def h(s: list[int]) -> int:
-		return int(sum([(16 - s[i]) * (abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3))) for i in range(16) if s[i] != 15]) * ratio)
+		return int(aug(sum([(16 - s[i]) * (abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3))) for i in range(16) if s[i] != 15])))
 
 	return h
 
 
-def misplaced(ratio: float):
+def misplaced(aug: Callable[[int], Union[int, float]]):
 	"""
 	错位数
 	"""
+
 	def h(s: list[int]) -> int:
-		return int(sum([1 for i in range(16) if s[i] != i and s[i] != 15]) * ratio)
+		return int(aug(sum([1 for i in range(16) if s[i] != i and s[i] != 15])))
 
 	return h
 
 
-def weighted_misplaced(ratio: float):
+def weighted_misplaced(aug: Callable[[int], Union[int, float]]):
 	"""
 	带权错位数，原理相同
 	"""
+
 	def h(s: list[int]) -> int:
-		return int(sum([s[i] for i in range(16) if s[i] != i and s[i] != 15]) * ratio)
+		return int(aug(sum([s[i] for i in range(16) if s[i] != i and s[i] != 15])))
 
 	return h
