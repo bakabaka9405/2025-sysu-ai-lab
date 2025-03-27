5be6a78c75df34d97f8e012b9d995a79c7db7833
bakabaka9405 <2677423642@qq.com>
2025-03-27 18:17:04 +0800

    docs: 为 lab3/assignment2 的四个启发式函数添加注释

--- lab3/assignment2/heuristic.py
+++ lab3/assignment2/heuristic.py
@@ -1,4 +1,7 @@
 def manhattan(ratio: float):
+	"""
+	曼哈顿距离
+	"""
 	def h(s: list[int]) -> int:
 		return int(sum([abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3)) for i in range(16) if s[i] != 15]) * ratio)
 
@@ -6,6 +9,10 @@ def manhattan(ratio: float):
 
 
 def weighted_manhattan(ratio: float):
+	"""
+	带权曼哈顿距离
+	理论上能刺激 A* 算法按 1,2,3... 的顺序还原 puzzle，从而提高在宽松步数限制下的计算效率
+	"""
 	def h(s: list[int]) -> int:
 		return int(sum([(16 - s[i]) * (abs((s[i] >> 2) - (i >> 2)) + abs((s[i] & 3) - (i & 3))) for i in range(16) if s[i] != 15]) * ratio)
 
@@ -13,6 +20,9 @@ def weighted_manhattan(ratio: float):
 
 
 def misplaced(ratio: float):
+	"""
+	错位数
+	"""
 	def h(s: list[int]) -> int:
 		return int(sum([1 for i in range(16) if s[i] != i and s[i] != 15]) * ratio)
 
@@ -20,6 +30,9 @@ def misplaced(ratio: float):
 
 
 def weighted_misplaced(ratio: float):
+	"""
+	带权错位数，原理相同
+	"""
 	def h(s: list[int]) -> int:
 		return int(sum([s[i] for i in range(16) if s[i] != i and s[i] != 15]) * ratio)
 
